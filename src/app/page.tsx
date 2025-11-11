5ebe54623dd76b735033742c3a101970760121cc
matheusauerswald01 <theus.tw00@gmail.com>
2025-11-11 15:57:24 -0400

    feat: add banner images and integrate them into the homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import { Header } from "@/components/common/header";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Header />
+
+      <div className="space-y-6 px-5">
+        <Image
+          src="/banner-01.png"
+          alt="Leve uma vida com estilo"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+
+        <Image
+          src="/banner-02.png"
+          alt="Seja Autentico"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
     </>
   );
 }
