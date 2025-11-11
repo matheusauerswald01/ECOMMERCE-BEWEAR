4d3c8d8b5dba8fc999acd1a4c0392750da0fbc68
matheusauerswald01 <theus.tw00@gmail.com>
2025-11-11 09:45:52 -0400

    feat: add authentication schema and integrate better-auth for user management

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,7 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import "dotenv/config";
+import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema,
+});
